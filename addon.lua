6ebe24cc59a0e99ce178f8ff6fc8e8bc88a3a13c
Tom Wieland <tom.wieland@gmail.com>
2010-04-15 07:19:41 +0200

    simplified the logic

--- addon.lua
+++ addon.lua
@@ -1,14 +1,15 @@
 if select(2, UnitClass('player')) ~= 'HUNTER' then return end
 
 local f = CreateFrame('Frame')
-f:SetScript('OnEvent', function (self, event, ...)
-	local unit = ...
-	if UnitAura(unit, 'Dazed') then
-		if unit == 'player' then
+f:SetScript('OnEvent', function (self, event, unit)
+	if
+		unit == 'player' or
+		unit:match('party.*') or
+		unit:match('raid.*')
+	then
+		if UnitDebuff(unit, 'Dazed') then
 			CancelUnitBuff('player', 'Aspect of the Pack')
 			CancelUnitBuff('player', 'Aspect of the Cheetah')
-		elseif unit:match('party.*') or unit:match('raid.*') then
-			CancelUnitBuff('player', 'Aspect of the Pack')
 		end
 	end
 end)
